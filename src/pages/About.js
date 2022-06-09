a21c44d988004847528ea9bc747fc42e7a379519
MattIkeW <107011569+MattIkeW@users.noreply.github.com>
2022-06-08 20:16:36 -0400

    created assets folder in src but still not compiling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import star_Anime from "../assets/Star_Anime.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p>
+          <div class="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                // Image goes here
+                star_Anime
+                alt="Profile Pic"
+                ></img>
+            </div> 
+          </div>
+          <div className="split right">
+          <div className="centered">
+            <div className="Matthew Wakhweya">Your Name</div>
+            <div className="Music fan and Koding King!!!">
+              // Details about you goes here
+            </div>
+          </div>
+        </div>
+        </p> 
       </div>
     )
   }
